437a3a7a2b8c6e387b7645d8989a417c266af8be
delta <d@qiscus.com>
2013-05-05 06:49:11 +0800

    initial wandrr

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wandrr::Application.config.secret_token = '1dd4200c5920529893d5e865f5cefc3541884c3e4131dde967f6826a2a5a2b5b3c0237f8d6b4064fbea97cf024dd99b98d6a5606ef508ebb036a1c0a01a8b8d8'
